44ba8cbb244c1ed3be618b6cf9417ec5a0090ca0
IgorOK96 <IgorOK96@users.noreply.github.com>
2024-10-27 13:48:52 +0100

    Commit custom Cell

---
+++ MyPlace/CustomTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlace
+//
+//  Created by user246073 on 10/27/24.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+}
