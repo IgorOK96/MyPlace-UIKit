f8c67285731843b8c7bf450c7eb413b4122f0a66
IgorOK96 <IgorOK96@users.noreply.github.com>
2024-10-27 13:28:56 +0100

    Commit - icon

--- MyPlace/MainViewController.swift
+++ MyPlace/MainViewController.swift
@@ -18,7 +18,6 @@ class MainViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.rowHeight = 120
         self.navigationItem.leftBarButtonItem = self.editButtonItem
     }
 
@@ -29,10 +28,11 @@ class MainViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        var content = cell.defaultContentConfiguration()
-        content.text = restaurantNames[indexPath.row]
-        content.image = UIImage(named: restaurantNames[indexPath.row])
-        cell.contentConfiguration = content
+        cell.textLabel?.text = restaurantNames[indexPath.row]
+        cell.imageView?.image = UIImage(named: restaurantNames[indexPath.row])
+        cell.imageView?.layer.cornerRadius = cell.frame.size.height / 2
+        cell.imageView?.clipsToBounds = true
+        
         return cell
     }
 
@@ -45,4 +45,12 @@ class MainViewController: UITableViewController {
         restaurantNames.insert(movedEmoji, at: destinationIndexPath.row)
         tableView.reloadData()
     }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        85
+    }
+
+
+// MARK: - Table View Delegate
+    
 }
